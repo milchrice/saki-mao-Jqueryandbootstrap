d61f9f085ff22efcdeb9b00cfe9271065dca57c1
meow0222 <meow222.csgo@gmail.com>
2023-08-02 10:45:18 +0900

    Change JS syntax to jQuery.

--- js/clickanduser.js
+++ js/clickanduser.js
@@ -1,68 +1,92 @@
-// user input&output
+
 function cafe() {
-  let greeting = "Hello!";
-  built 
-  console.log(typeof(greeting)); 
+    let greeting = "Hello!";
+    console.log(typeof (greeting));
 
-  const now = new Date();
-  console.log(typeof(now)); 
+    const now = new Date();
+    console.log(typeof (now));
 
-  const hour = now.getHours();
+    const hour = now.getHours();
 
-  if (hour >= 5 && hour < 12) {
-      greeting = "Good Morning!";
-  } else if (hour >= 12 && hour < 18) {
-      greeting = "Good Afternoon!";
-  } else {
-      greeting = "Good Evening!";
-  }
+    if (hour >= 5 && hour < 12) {
+        greeting = "Good Morning!";
+    } else if (hour >= 12 && hour < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
 
-  while (true) {
-      let answer = window.prompt(`${greeting} Which do you like?\n\nCoffee\nBread\nEclair`);
-  
-      if (answer === "Coffee") {
-          window.alert("You should take a look Coffee Cafe");
-          return; 
-      } else if (answer === "Bread") {
-          window.alert("We recommend Bread Cafe");
-          return; 
-      } else if (answer === "Eclair") {
-          window.alert("We recommend Eclair Cafe");
-          return; 
-      } else { 
-          window.alert("Please type some dessert name");
-      }
-  }
+    while (true) { //Return Statement, While Loop, Input-Output
+        let answer = window.prompt(`${greeting} Which do you like?\n\nCoffee\nBread\nEclair`);
+
+        if (answer === "Coffee") {
+            window.alert("You should take a look Coffee Cafe");
+            return;
+        } else if (answer === "Bread") {
+            window.alert("We recommend Bread Cafe");
+            return;
+        } else if (answer === "Eclair") {
+            window.alert("We recommend Eclair Cafe");
+            return;
+        } else {
+            window.alert("Please type some dessert name");
+        }
+    }
 };
 
 cafe();
 
 // visitor
-// 訪問回数のカウントと表示:
 function showVisitCount() {
-  // showVisitCount()関数の定義を開始します。この関数は訪問回数をカウントして表示する機能を持ちます。
-  let visitCount = 0;
-  // 変数visitCountを宣言し、0で初期化します。この変数は訪問回数を格納するために使用されます。
-  const storedVisitCount = localStorage.getItem('visitCount');
+    let visitCount = 0;
 
-  // localStorageから"visitCount"というキーで保存されている訪問回数を取得します。getItem()メソッドを使用してローカルストレージから値を取得し、変数storedVisitCountに格納します。
-  if (storedVisitCount) {
-      visitCount = parseInt(storedVisitCount);
-  }
+    const storedVisitCount = localStorage.getItem('visitCount');
 
-  // storedVisitCountがnullまたはundefinedでない場合、つまりローカルストレージに訪問回数が保存されている場合は、parseInt()関数を使ってstoredVisitCountの値を整数に変換し、visitCountに代入します。
-  for (let i = 0; i < 1; i++) {
-      visitCount++;
-  }
+    if (storedVisitCount) {
+        visitCount = parseInt(storedVisitCount);
+    }
 
-  // forループを使用して、訪問回数を1回増やします。現在のコードでは1回のみですが、必要に応じてループ回数を変更できます。
-  localStorage.setItem('visitCount', visitCount);
+    for (let i = 0; i < 1; i++) { //For Loop:
+        visitCount++;
+    }
 
-  // localStorageに"visitCount"というキーで訪問回数を保存します。これにより、ページを閉じて再度訪れても訪問回数が保持されます。
-  const counterElement = document.getElementById('counter');
-  // HTMLの要素を取得するために、getElementById()メソッドを使ってcounterというIDを持つ要素を取得し、変数counterElementに格納します。
-  counterElement.textContent = `You are the\n${visitCount} visitor.。`;
+    localStorage.setItem('visitCount', visitCount);
+
+    const counterElement = document.getElementById('counter');
+    counterElement.textContent = `You are the\n${visitCount} visitor.。`;
 }
 
-// 訪問者数をHTMLの要素に表示します。textContentプロパティを使って訪問者数を表示します。テンプレートリテラルを使って文字列を組み立てています。
 window.onload = showVisitCount;
+
+
+function setupPopup(buttonId, wrapperId, closeId) {
+    // {: setupPopupという名前の関数を定義しています。この関数は3つの引数を受け取ります：buttonId、
+    // ：buttonId、wrapperId、closeId。それぞれは文字列で、HTML要素のIDを表します
+    const clickBtn = document.getElementById(buttonId);
+    // clickBtnという変数に、buttonIdで指定されたIDを持つHTML要素（ボタン）を取得して格納しています。
+    const popupWrapper = document.getElementById(wrapperId);
+    // という変数に、wrapperIdで指定されたIDを持つHTML要素（ポップアップのラッパー）を取得して格納しています。
+
+    clickBtn.addEventListener('click', () => {
+        popupWrapper.style.display = 'block';
+    });
+    // : ボタンがクリックされたときに実行するイベントリスナーを設定しています。ボタンがクリックされると、関数が呼び出されます。
+    // ポップアップのラッパー要素のdisplayプロパティを'block'に設定しています。これにより、ポップアップが表示されます
+
+    popupWrapper.addEventListener('click', e => {
+        // ポップアップのラッパー要素に対してクリックイベントリスナーを設定しています。
+        if (e.target.id === wrapperId || e.target.id === closeId) {
+            // イベントが発生した要素のID（e.target.id）をチェックしています。もしイベントがポップアップのラッパー要素をクリックした場合（wrapperId）または閉じるボタンをクリックした場合（closeId）、ポップアップを非表示にする処理が実行されます。
+            popupWrapper.style.display = 'none';
+            // 'none';: ポップアップのラッパー要素のdisplayプロパティを'none'に設定しています。これにより、ポップアップが非表示になります。
+        }
+    });
+}
+
+setupPopup('pin1', 'popup-wrapper1', 'close');
+setupPopup('pin2', 'popup-wrapper2', 'close');
+setupPopup('pin3', 'popup-wrapper3', 'close');
+  // setupPopup('pin1', 'popup-wrapper1', 'close');: setupPopup関数を呼び出して、IDが'pin1'のボタンをクリックしたときに、IDが'popup-wrapper1'のポップアップを表示し、IDが'close'の要素をクリックするとポップアップを閉じるように設定しています。
+  
+  // 同様に、setupPopup('pin2', 'popup-wrapper2', 'close');とsetupPopup('pin3', 'popup-wrapper3', 'close');を呼び出して、他のボタンとポップアップに対して同じ処理を行っています。
+  
--- js/mao.js
+++ js/mao.js
@@ -1,24 +1,71 @@
-const img = ["imgs/cafe2.jpg", "imgs/cafe3.jpg", "imgs/cafe4.jpg", "imgs/cafe5.jpg"];
-const changePic = document.getElementById('changePic'); // 画像要素を取得
-let count = 0;
+$(document).ready(function () {
+  const img = ["imgs/cafe2.jpg", "imgs/cafe3.jpg", "imgs/cafe4.jpg", "imgs/cafe5.jpg"];
+  const changePic = $('#changePic');
+  let count = 0;
 
-// picChange 関数のみ修正
-function picChange() {
-  // 画像選択
-  changePic.src = img[count];
+  function picChange() {
+    changePic.attr('src', img[count]);
+    count++;
+    if (count === img.length) {
+      count = 0;
+    }
+  }
 
-  count++;
-  if (count === img.length) {
-    count = 0; // 最後の画像の後は最初の画像に戻る
+  setInterval(picChange, 5000);
+});
+
+
+
+class ImageCarousel {
+  constructor(list, imgElement, rightArrow, leftArrow, pElement) {
+    this.list = list;
+    this.imgElement = imgElement;
+    this.rightArrow = rightArrow;
+    this.leftArrow = leftArrow;
+    this.pElement = pElement;
+    this.imgN = 0;
+
+    this.setImage();
+
+    $(document).ready(() => {
+      this.setImage();
+    });
+
+    this.rightArrow.on('click', () => {
+      this.imgN -= 1;
+      if (this.imgN < 0) {
+        this.imgN = this.list.length - 1;
+      }
+      this.setImage();
+    });
+
+    this.leftArrow.on('click', () => {
+      this.imgN += 1;
+      if (this.imgN >= this.list.length) {
+        this.imgN = 0;
+      }
+      this.setImage();
+    });
+  }
+
+  setImage() {
+    this.imgElement.attr('src', this.list[this.imgN]);
+    this.pElement.text(`${this.imgN + 1} / ${this.list.length}`);
   }
 }
 
-setInterval(picChange, 5000);
+$(document).ready(function () {
+  const list1 = ['imgs/01.jpg', 'imgs/02.jpg', 'imgs/03.jpg'];
+  const carousel1 = new ImageCarousel(list1, $('#main-pic1'), $('#next1'), $('#prev1'), $('#photo1'));
 
+  const list2 = ['imgs/04.jpg', 'imgs/05.jpg', 'imgs/06.jpg'];
+  const carousel2 = new ImageCarousel(list2, $('#main-pic2'), $('#next2'), $('#prev2'), $('#photo2'));
 
+  const list3 = ['imgs/07.jpg', 'imgs/08.jpg', 'imgs/09.jpg'];
+  const carousel3 = new ImageCarousel(list3, $('#main-pic3'), $('#next3'), $('#prev3'), $('#photo3'));
+});
 
-
-
+/*
 // JavaScript
 class ImageCarousel {
   constructor(list, imgElement, rightArrow, leftArrow, pElement) {
@@ -113,3 +160,4 @@ setupPopup('pin3', 'popup-wrapper3', 'close');
 // setupPopup('pin1', 'popup-wrapper1', 'close');: setupPopup関数を呼び出して、IDが'pin1'のボタンをクリックしたときに、IDが'popup-wrapper1'のポップアップを表示し、IDが'close'の要素をクリックするとポップアップを閉じるように設定しています。
 
 // 同様に、setupPopup('pin2', 'popup-wrapper2', 'close');とsetupPopup('pin3', 'popup-wrapper3', 'close');を呼び出して、他のボタンとポップアップに対して同じ処理を行っています。
+*/
